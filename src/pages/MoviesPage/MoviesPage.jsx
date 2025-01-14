55f245ff13f70b0bcde0077ff0346b6fd50e9837
Alexandra0707 <alex07marta@gmail.com>
2025-01-14 16:41:47 +0000

    gfgf

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,29 +1,73 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { searchMovies } from "../../services/api";
 import MovieList from "../../components/MovieList/MovieList";
-import { searchMovies } from "../../api/tmdb-api";
+import { Field, Form, Formik } from "formik";
+import Loader from "../../components/Loader/Loader";
+import s from "./MoviesPage.module.css";
 
 const MoviesPage = () => {
+  const initialValues = { query: "" };
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  const handleSearch = async (e) => {
-    e.preventDefault();
-    const results = await searchMovies(query);
-    setMovies(results);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const queryParam = searchParams.get("query");
+
+  const handleSearchSubmit = (values, actions) => {
+    const searchQuery = values.query.trim().toLowerCase();
+    if (searchQuery === query) return;
+    setQuery(searchQuery);
+    setSearchParams({ query: searchQuery });
+    setMovies([]);
+    actions.resetForm();
   };
+
+  useEffect(() => {
+    if (!queryParam) return;
+
+    const fetchMovies = async () => {
+      setIsLoading(true);
+      setError(null);
+
+      try {
+        const { results } = await searchMovies(queryParam);
+        setMovies(results || []);
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+        setError("Failed to fetch movies. Please try again.");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchMovies();
+  }, [queryParam]);
 
   return (
     <div>
-      <form onSubmit={handleSearch}>
-        <input
-          type="text"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          placeholder="Search movies..."
-        />
-        <button type="submit">Search</button>
-      </form>
-      <MovieList movies={movies} />
+      <Formik initialValues={initialValues} onSubmit={handleSearchSubmit}>
+        <Form className={s.form}>
+          <Field
+            type="text"
+            placeholder="Search movies..."
+            name="query"
+            className={s.field}
+          />
+          <button type="submit" className={s.btn}>
+            Search
+          </button>
+        </Form>
+      </Formik>
+
+      {isLoading && <Loader />}
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      {!isLoading && movies.length === 0 && queryParam && (
+        <p>No movies found for {queryParam}.</p>
+      )}
+      {movies.length > 0 && <MovieList movies={movies} />}
     </div>
   );
 };
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const API_Key = import.meta.env.VITE_API_KEY;
 
-axios.defaults.baseURL = "https://api.themoviedb.org/3/";
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
 
 const options = {
   headers: {
