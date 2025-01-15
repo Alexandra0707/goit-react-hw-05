795f6bb2467c239e0ab46ab28791cce324b5e465
Alexandra0707 <alex07marta@gmail.com>
2025-01-15 16:36:56 +0000

    appi

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
-const API_Key = import.meta.env.VITE_API_KEY;
+const API_Key = `f4f114951939468c2b77438140c3fb0c`;
 
-axios.defaults.baseURL = "https://api.themoviedb.org/3";
+axios.defaults.baseURL = "https://api.themoviedb.org/3/";
 
 const options = {
   headers: {
-    Authorization: `Bearer ${API_Key}`,
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNGYxMTQ5NTE5Mzk0NjhjMmI3NzQzODE0MGMzZmIwYyIsIm5iZiI6MTczNjg3MDIwNS43ODU5OTk4LCJzdWIiOiI2Nzg2ODkzZDJiMWU0MGU4M2ZiYjE0NGYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Nz2ib_8UAY_-BsqKHqLwVqtfAkRXm63d53dWD7ScRPE`,
   },
 };
 export const fetchTrendingMovies = async () => {
